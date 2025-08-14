bd034cbe9ec769003142511d3a162fa205f75d16
napu <nahuel.haberkorn@gmail.com>
2025-08-13 23:41:18 -0300

    text indentation feature

--- src/components/split-text.js
+++ src/components/split-text.js
@@ -7,19 +7,44 @@ import { duration, staggerAmount, customEaseName } from "../utils/variables.js";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 document.fonts.ready.then(() => {
-  const texts = document.querySelectorAll("[data-split-text]");
+  const texts = document.querySelectorAll("[data-split-text='true']");
 
   texts.forEach((el) => {
     el.innerHTML = el.innerHTML.replace(/\n/g, "<br>");
   });
 
   SplitText.create(texts, {
-    autoSplit: true,
-    type: "lines",
-    mask: "lines",
-    linesClass: "line",
-    aria: "none",
-  });
+  autoSplit: true,
+  type: "lines",
+  mask: "lines",
+  linesClass: "line",
+  prepareText: (text, el) => {
+    let indent = el.dataset.indent || "";
+    if (!indent) return text;
+
+    // Fill missing values with the last available value
+    let parts = indent.split(",").map(Number);
+    while (parts.length < 4) {
+      parts.push(parts[parts.length - 1]);
+    }
+    
+    const [desktop, tablet, landscape, portrait] = parts;
+    const breakpoints = { desktop, tablet, landscape, portrait };
+    console.log(breakpoints)
+
+    // Select indent key by viewport width
+    let key;
+    if (window.innerWidth > 991) key = "desktop";
+    else if (window.innerWidth > 767) key = "tablet";
+    else if (window.innerWidth > 479) key = "landscape";
+    else key = "portrait";
+
+    const spaces = "\u00A0".repeat(breakpoints[key]);
+    return spaces + text;
+  },
+});
+
+
 
   texts.forEach((textElement) => {
     if (textElement.dataset.autoplay !== "true") return;
