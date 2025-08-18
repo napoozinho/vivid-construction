68099df860c6f237be6fe4da3deb3ce7cf09c400
napu <nahuel.haberkorn@gmail.com>
2025-08-18 00:06:44 -0300

    added lenis

---
+++ src/components/lenis.js
@@ -0,0 +1,16 @@
+import Lenis from 'lenis'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import 'lenis/dist/lenis.css'
+
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis()
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000)
+})
+
+lenis.on('scroll', () => {
+  ScrollTrigger.update()
+})
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import "./components/navbar.js";
+//global
+import "./components/lenis.js";
 import "./components/split-text.js";
-import "./components/image-reveal.js";
+import "./components/navbar.js";
 import "./components/link-reveal.js";
-import "./components/custom-cursor.js";
-
-import "./components/projects-mask.js";
+import "./components/image-reveal.js";
 
 import "./components/home-hero.js";
+import "./components/projects-mask.js";
+import "./components/custom-cursor.js";
+
+
