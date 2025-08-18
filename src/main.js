f491563d2819aa1083d059fa3725472a292abf12
napu <nahuel.haberkorn@gmail.com>
2025-08-17 22:13:16 -0300

    updated link animation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./components/split-text.js";
 import "./components/image-reveal.js";
-import "./components/button.js";
+import "./components/link-reveal.js";
 import "./components/custom-cursor.js";
 import "./components/projects-mask.js";
 
