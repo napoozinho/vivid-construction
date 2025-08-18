8aaade26e3062057edb5f9441905e52bc898bbba
napu <nahuel.haberkorn@gmail.com>
2025-08-17 23:49:33 -0300

    navbar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+import "./components/navbar.js";
 import "./components/split-text.js";
 import "./components/image-reveal.js";
 import "./components/link-reveal.js";
 import "./components/custom-cursor.js";
+
 import "./components/projects-mask.js";
 
 import "./components/home-hero.js";
