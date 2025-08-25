10528a7a36cec4a72361e7216dbb8eecfe765700
napu <nahuel.haberkorn@gmail.com>
2025-08-25 01:37:15 -0300

    updated duration variable

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -6,7 +6,7 @@ gsap.registerPlugin(CustomEase);
 CustomEase.create("ease-primary", "0.62, 0.05, 0.01, 0.99");
 CustomEase.create("ease-secondary", "0.16, 1, 0.35, 1");
 
-export const duration = 1.47;
+export const duration = 1.2;
 export const staggerAmount = 0.2;
 export const easePrimary = "ease-primary";
 export const easeSecondary = "ease-secondary";
