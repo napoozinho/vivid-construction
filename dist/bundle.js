e349da2b07c4de8bf0f2e45d36ce40ac04f40e59
iamMarcSanchez <122655646+iamMarcSanchez@users.noreply.github.com>
2026-07-21 17:01:33 +0800

    Update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -7801,7 +7801,7 @@ gsapWithCSS.registerPlugin(ScrollTrigger);
 
 const setDifference = () =>
   gsapWithCSS.set(blendTargets, {
-    mixBlendMode: "normal"
+    mixBlendMode: "difference"
   });
 
     
